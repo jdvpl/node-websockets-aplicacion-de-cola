15ed154139e873b72f9aa8299d806c75c8e98412
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-10 20:39:18 -0500

    ATENder ticket, siguiente ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const fs= require('fs');
 
+class Ticket{
+  constructor(numero,escritorio) {
+    this.numero=numero;
+    this.escritorio=escritorio;
+  }
+}
 class TicketControl{
 
   constructor() {
@@ -37,6 +43,36 @@ class TicketControl{
   saveDb() {
     const dbpath = path.join(__dirname, '../data/data.json');
     fs.writeFileSync(dbpath,JSON.stringify(this.toJson))
+  }
+
+  netTicket(){
+    this.ultimo+=1;
+    const ticket=new Ticket(this.ultimo,null);
+    this.tickets.push(ticket);
+
+    this.saveDb();
+    return 'Ticket'+ ticket.numero;
+  }
+
+  attendTicket(escritorio){
+    // no tenemos tickets
+    if(this.tickets.length===0){
+      return null;
+    }
+    // borrar el ticket atendido
+    const ticket=this.tickets.shift();
+    
+    ticket.escritorio=escritorio;
+
+    this.ultimos4.unshift(ticket);
+
+    if(this.ultimos4.length>4){
+      this.ultimos4.splice(-1,1)
+    }
+
+    this.saveDb();
+
+    return ticket;
   }
   
 }
