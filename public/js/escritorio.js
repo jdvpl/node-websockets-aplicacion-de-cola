ddf5dd75ad0ed94b0b2f51f122411bc78857a82b
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-10 21:42:32 -0500

    saber en que escritorio estamos

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,38 @@
+const searchParams = new URLSearchParams(window.location.search);
+
+// /referencias html
+const lblEscritorio=document.querySelector('h1');
+const btnAtender=document.querySelector('button');
+
+if(!searchParams.has('escritorio')){
+  window.location='index.html';
+  throw new Error('El escritorio es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio');
+
+lblEscritorio.innerText=escritorio;
+
+const socket = io();
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled=false;
+    
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+    btnAtender.disabled=true;
+});
+
+socket.on('ultimo-ticket',ultimo=>{
+  // lblNuevoTicket.innerHTML='Ticket '+ultimo;
+});
 
 
-console.log('Escritorio HTML');
+btnAtender.addEventListener( 'click', () => {    
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
